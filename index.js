f8c4d534d4217371d6f25184d845589ea0e6850b
Stefan Heldt <stefan.heldt@holisticon.de>
2017-12-22 14:55:17 +0100

    basic index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const https = require('https');
 
 const APP_ID = 'amzn1.ask.skill.50843b7a-5cb1-4288-b387-bc53186440c7';  // TODO replace with your app ID (OPTIONAL).
 
-const currentSong = (emit) => {
-  https.get('https://www.byte.fm/ajax/song-history/', (resp) => {
+const currentSong = function(emit) {
+  https.get('https://www.byte.fm/ajax/song-history/', function(resp) {
     let data = '';
 
     // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
+    resp.on('data', function(chunk) {
       data += chunk;
     });
 
     // The whole response has been received. Print out the result.
-    resp.on('end', () => {
+    resp.on('end', function() {
       console.log(JSON.parse(data).current_show_title);
       const title = JSON.parse(data).tracks[0].replace('&ndash;', '-');
       const ssmlTitle = `<prosody rate="slow">${title}</prosody>`;
@@ -26,28 +26,28 @@ const currentSong = (emit) => {
       emit(':tellWithCard', ssmlTitle, "Aktueller Song", title, image);
     });
 
-  }).on("error", (err) => {
+  }).on("error", function(err) {
     console.log("Error: " + err.message);
   });
 };
 
 const handlers = {
-  'AMAZON.StopIntent': () => {
+  'AMAZON.StopIntent': function() {
     this.emit(':tell', 'OK');
   },
-  'AMAZON.CancelIntent': () => {
+  'AMAZON.CancelIntent': function() {
     this.emit(':tell', 'OK');
   },
-  'AMAZON.HelpIntent': () => {
+  'AMAZON.HelpIntent': function() {
     this.emit(':tell', 'Frag mich nach dem aktuellen Titel');
   },
-  'CurrentSongIntent': () =>{
+  'CurrentSongIntent': function() {
     currentSong(this.emit);
   }
 };
 
 
-exports.handler = (event, context) => {
+exports.handler = function(event, context) {
   const alexa = Alexa.handler(event, context);
   alexa.APP_ID = APP_ID;
   alexa.registerHandlers(handlers);
