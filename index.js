fa523a86e0bf157b178cc387f6e24ae763617dd6
Christoph Gerkens <ch.gerkens@gmail.com>
2017-12-27 19:25:51 +0000

    use entity resolution for station slot

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const tellAlexa = function(song, env) {
 };
 
 const currentSong = function (env) {
-  const key = env.event.request.intent.slots.Station.value.trim().replace(/ /g, '').replace(/\./g, '').toLowerCase();
+  const slotValues = getSlotValues(env.event.request.intent.slots);
+  const key = slotValues.Station.resolved.toLowerCase();
   console.log(key);
   const stationCartridge = "./cartridges/" + key + ".js";
   try {
@@ -32,6 +33,52 @@ const currentSong = function (env) {
     env.emit(':tellWithCard',  responseMessage, 'Sender unbekannt', responseMessage);
   }
 };
+
+function getSlotValues (filledSlots) {
+    //given event.request.intent.slots, a slots values object so you have
+    //what synonym the person said - .synonym
+    //what that resolved to - .resolved
+    //and if it's a word that is in your slot values - .isValidated
+    let slotValues = {};
+
+    console.log('The filled slots: ' + JSON.stringify(filledSlots));
+    Object.keys(filledSlots).forEach(function(item) {
+        //console.log("item in filledSlots: "+JSON.stringify(filledSlots[item]));
+        var name = filledSlots[item].name;
+        //console.log("name: "+name);
+        if(filledSlots[item]&&
+           filledSlots[item].resolutions &&
+           filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
+           filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
+           filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code ) {
+
+            switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+                case "ER_SUCCESS_MATCH":
+                    slotValues[name] = {
+                        "synonym": filledSlots[item].value,
+                        "resolved": filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
+                        "isValidated": true
+                    };
+                    break;
+                case "ER_SUCCESS_NO_MATCH":
+                    slotValues[name] = {
+                        "synonym": filledSlots[item].value,
+                        "resolved": filledSlots[item].value,
+                        "isValidated":false
+                    };
+                    break;
+                }
+            } else {
+                slotValues[name] = {
+                    "synonym": filledSlots[item].value,
+                    "resolved": filledSlots[item].value,
+                    "isValidated": false
+                };
+            }
+        },this);
+        //console.log("slot values: "+JSON.stringify(slotValues));
+        return slotValues;
+}
 
 const handlers = {
   'AMAZON.StopIntent': function () {
