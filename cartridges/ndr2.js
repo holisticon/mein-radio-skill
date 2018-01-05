bba8afb14d3c648ae9ddad1bcee45c7311167e4c
Stefan Heldt <stefan.heldt@holisticon.de>
2018-01-05 09:59:57 +0100

    fixed ndr2 and added test JSONs

--- cartridges/ndr2.js
+++ cartridges/ndr2.js
@@ -1,22 +1,30 @@
-const http = require('http');
+const https = require('https');
 
 const station = function(callback, env) {
-    http.get('http://www.ndr.de/public/radio_playlists/ndr2.json', function (result) {
-        result.setEncoding('utf8');
-        result.on('data', function (chunk) {
-            const json = JSON.parse(chunk);
+    https.get('https://www.ndr.de/public/radioplaylists/ndr2.json', function (resp) {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', function (chunk) {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', function () {
+            const json = JSON.parse(data);
             console.log(json);
             const song = {
                 "title": json.song_now,
                 "image": {
-                    smallImageUrl: 'https://www.ndr.de/public/radio_playlists/coverimages/' + json.song_now_cover + '_300x300.jpg',
-                    largeImageUrl: 'https://www.ndr.de/public/radio_playlists/coverimages/' + json.song_now_cover + '_600x600.jpg'
+                    "smallImageUrl": 'https://www.ndr.de/public/radio_playlists/coverimages/' + json.song_now_cover + '_300x300.jpg',
+                    "largeImageUrl": 'https://www.ndr.de/public/radio_playlists/coverimages/' + json.song_now_cover + '_600x600.jpg'
                 }
             }
             callback(song, env);
         });
-    }).on('error', function (err) {
-        console.log('Error: ' + err.message);
+
+    }).on("error", function (err) {
+        console.log("Error: " + err.message);
     });
 }
 
